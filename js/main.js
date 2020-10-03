e202cf0e80553002b2a96d8c803ff20adc6057c7
hebraheem <iadebiyi01@gmail.com>
2020-10-03 07:55:21 +0100

    modified computer

--- js/main.js
+++ js/main.js
@@ -19,13 +19,23 @@ function Choose(){
 
 
 let playerbtn = document.querySelectorAll(".btn")[0];
+// let playerbtn1 = document.querySelectorAll(".btn")[1];
 playerbtn.addEventListener("click", rollOne);
+// playerbtn1.addEventListener("click", rollOne);
 function rollOne() {
     let randomNumber = Math.floor(Math.random()*6)+ 1;
     let randomDiceImage = "dice" + randomNumber + ".png";
     let randomImageSource = "images/" + randomDiceImage;
     let image = document.querySelectorAll("img")[0];
     image.setAttribute("src",randomImageSource);
+
+
+   
+    // let randomNumber1 = Math.floor(Math.random()*6)+ 1;
+    // let randomDiceImage1 = "dice" + randomNumber1 + ".png";
+    // let randomImageSource1 = "images/" + randomDiceImage1;
+    // let image1 = document.querySelectorAll("img")[1];
+    // image1.setAttribute("src",randomImageSource1);
 }
 
 let playerbtn1 = document.querySelectorAll(".btn")[1];
@@ -51,4 +61,22 @@ function rollThree() {
     let randomImageSource3 = "images/" + randomDiceImage3;
     let image3 = document.querySelectorAll("img")[3];
     image3.setAttribute("src",randomImageSource3);
+
+
+   
+    if (randomNumber2 > randomNumber3){
+        document.querySelector(".player3").innerHTML = "Player Wins";
+        document.querySelector(".compute").innerHTML = "loose";
+
+
+    } else if(randomNumber3 > randomNumber2) {
+        document.querySelector(".player3").innerHTML = "loose";
+        document.querySelector(".compute").innerHTML = "Computer Wins";
+    }
+    else {
+        document.querySelector(".player3").innerHTML ="Draw";
+        document.querySelector(".compute").innerHTML = "Draw";
+    }
 }
+
+
